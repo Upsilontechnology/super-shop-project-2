b2fe8a6b074145341bd3b0f037efd9cbc6968818
Upsilontechnology <hello.upsilon@gmail.com>
2024-04-16 02:18:35 +0600

    all code added

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MainLayout = () => {
     return (
         <div className='text-black'>
-            <h1>Hello World</h1>
+            <h1>Abu Sayeed</h1>
         </div>
     );
 };
