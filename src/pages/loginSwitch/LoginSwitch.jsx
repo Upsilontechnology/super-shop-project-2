d1788be479ac7ce8ab0ddf9a4360075f26f35956
MoRsHeD <mdmorshed0187@gmail.com>
2024-04-22 00:32:58 +0600

    loginswitch updated

--- src/pages/loginSwitch/LoginSwitch.jsx
+++ src/pages/loginSwitch/LoginSwitch.jsx
@@ -8,8 +8,16 @@ const LoginSwitch = () => {
                 <div className=' flex flex-col w-full 4xl:py-20 py-10 md:w-[768px] '>
                     <div className='bg-[#ebedfe] rounded-lg  py-[10%] px-[5%]  md:px-[15%]'>
                         <div className="flex gap-4 flex-col md:flex-row mt-8">
-                            <button className="text-xl rounded-md font-semibold  py-4 bg-[#757ec9] text-white hover:bg-[#565fa8] w-full">Log In as Admin</button>
-                            <button className="text-xl rounded-md font-semibold  py-4 bg-[#959ff0] text-white hover:bg-[#565fa8] w-full">Log In as Employee</button>
+                            <button className="text-xl rounded-md font-semibold  py-4 bg-[#757ec9] text-white hover:bg-[#565fa8] w-full">
+                                <Link to="/login">
+                                    Log In as Admin
+                                </Link>
+                            </button>
+                            <button className="text-xl rounded-md font-semibold  py-4 bg-[#959ff0] text-white hover:bg-[#565fa8] w-full">
+                                <Link to="/login">
+                                    Log In as Employee
+                                </Link>
+                            </button>
                         </div>
                         <div className='mt-16 text-center '>
                             <p>Do not have an account? <Link className='font-bold text-[#6486FD]' to='/register'>Register Now</Link></p>
