cd69a96f0598617d1c9f5fff119b0b9fe91c0155
Upsilontechnology <hello.upsilon@gmail.com>
2024-04-21 21:18:17 +0600

    all code added

---
+++ src/components/AuthProvider/AuthProvider.jsx
@@ -0,0 +1,64 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from '../../firebase/firebase.config';
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    const [loading, setLoading] = useState(true);
+    const provider = new GoogleAuthProvider();
+
+    // creating user
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    // sign in user
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // track the user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser);
+            console.log('current user in auth Provider', currentUser);
+            setLoading(false);
+        });
+        return () => {
+            return unsubscribe;
+        }
+    }, []);
+
+    // log out user
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    // google sign in
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    }
+    console.log(loading);
+    const authInfo = {
+        user,
+        loading,
+        setLoading,
+        createUser,
+        signInUser,
+        logOut,
+        googleSignIn,
+    };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
