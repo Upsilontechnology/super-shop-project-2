03c432777544c9464e21548baae96235d0151fa8
TahidulAlam <tahid.cse@gmail.com>
2024-04-30 01:27:19 +0600

    update

---
+++ src/Routes/EmployeeRouter.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import useEmployee from "../hooks/useEmployee";
+import useAuth from "../hooks/useAuth";
+
+const EmployeeRouter = () => {
+  const { user, loading } = useAuth();
+  const [isEmployee, isEmployeeLoading] = useEmployee();
+  const location = useLocation();
+
+  if (loading || isEmployeeLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-dots loading-lg "></span>
+      </div>
+    );
+  }
+
+  if (user && isEmployee) {
+    return children;
+  }
+  return <Navigate to="/" state={{ from: location }} replace></Navigate>;
+};
+
+export default EmployeeRouter;
