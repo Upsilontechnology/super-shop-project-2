14272ab1bb55f92a10e6c3f67bbac15c49fbce0e
Upsilontechnology <hello.upsilon@gmail.com>
2024-05-26 20:51:02 +0600

    all code added

--- src/hooks/useAxiosPrivate.jsx
+++ src/hooks/useAxiosPrivate.jsx
@@ -4,7 +4,7 @@ import auth from "../firebase/firebase.config";
 
 const instance = axios.create({
   //   baseURL: "",
-  baseURL: "https://super-shop-server-2.vercel.app/api",
+  baseURL: "http://localhost:5000/api",
   withCredentials: true,
 });
 
